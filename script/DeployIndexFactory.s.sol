f0757bc0bd30c55ce052cc753e3e3e6a97c42dca
soheilprs <soheilparsaei19@protonmail.com>
2025-05-05 16:50:05 +0330

    Added deployment script for factory && factory storage

---
+++ script/DeployIndexFactory.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/Test.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+import "../src/factory/IndexFactory.sol";
+
+contract DeployIndexFactory is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        string memory targetChain = "sepolia";
+        // string memory targetChain = "arbitrum_mainnet";
+
+        address owner = vm.addr(deployerPrivateKey);
+
+        if (keccak256(bytes(targetChain)) == keccak256("sepolia")) {} else if (
+            keccak256(bytes(targetChain)) == keccak256("arbitrum_mainnet")
+        ) {} else {
+            revert("Unsupported target chain");
+        }
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        address proxy = Upgrades.deployTransparentProxy(
+            "IndexFactory.sol",
+            owner,
+            abi.encodeCall(IndexFactory.initialize, (address(0), address(0), address(0), address(0), address(0)))
+        );
+
+        IndexFactory indexFactoryImplementation = IndexFactory(proxy);
+
+        address proxyAdmin = Upgrades.getAdminAddress(proxy);
+
+        console.log("IndexFactory implementation deployed at:", address(indexFactoryImplementation));
+        console.log("IndexFactory proxy deployed at:", address(proxy));
+        console.log("ProxyAdmin for IndexFactory deployed at:", address(proxyAdmin));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ script/DeployIndexFactoryStorage.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/Test.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+import "../src/factory/IndexFactoryStorage.sol";
+
+contract DeployIndexFactoryStorage is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        // string memory targetChain = "sepolia";
+
+        address owner = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        address proxy = Upgrades.deployTransparentProxy(
+            "IndexFactoryStorage.sol",
+            owner,
+            abi.encodeCall(IndexFactoryStorage.initialize, (address(0), address(0), address(0), false, address(0)))
+        );
+
+        IndexFactoryStorage indexFactoryStorageImplementation = IndexFactoryStorage(proxy);
+        address proxyAdmin = Upgrades.getAdminAddress(proxy);
+
+        console.log("IndexFactoryStorage implementation deployed at:", address(indexFactoryStorageImplementation));
+        console.log("IndexFactoryStorage proxy deployed at:", address(proxy));
+        console.log("ProxyAdmin for IndexFactoryStorage deployed at:", address(proxyAdmin));
+
+        vm.stopBroadcast();
+    }
+}
