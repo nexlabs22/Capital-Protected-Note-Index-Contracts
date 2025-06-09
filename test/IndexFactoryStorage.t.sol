63424f9073bd631a23b17e5b6060d396f9d81204
soheilprs <soheilparsaei19@protonmail.com>
2025-06-09 19:56:39 +0330

    Updated

--- test/IndexFactoryStorage.t.sol
+++ test/IndexFactoryStorage.t.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.25;
 import "forge-std/Test.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
 import {IndexToken} from "../src/token/IndexToken.sol";
 import {StagingCustodyAccount} from "../src/SCA/StagingCustodyAccount.sol";
 import {IndexFactoryStorage} from "../src/factory/IndexFactoryStorage.sol";
